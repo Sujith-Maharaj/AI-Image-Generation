3b9b40da18a36cd5e4eb7e2ffe4f03e121cf75c3
Sujith <sujith.maharaj02@gmail.com>
2024-03-05 17:52:11 +0530

    refactored same for deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () =>
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://ai-image-generation-1scs.onrender.com/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
